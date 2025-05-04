d12b6042adf52c32124eff79061677983c1dcae7
apavlovichev <apavlovichev@nota.tech>
2025-05-04 22:16:06 +0300

    Переписан метод main: - добавлен ввод двух целочисленных чисел - добавлены переменные результатов суммы, разности, произведения и частного введенных чисел - добавлен вывод в консоль результатов операций

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма введенных чисел равна " + sum);
+        System.out.println("Разность введенных чисел равна " + difference);
+        System.out.println("Произведение введенных чисел равно " + product);
+        System.out.println("Частное введенных чисел равно " + quotient);
     }
 }
