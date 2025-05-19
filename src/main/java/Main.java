c6bb48681052b5ccfa3e26de551902231c53affd
apavlovichev <apavlovichev@nota.tech>
2025-05-19 14:54:57 +0300

    Переписан метод main: - удалены методы ввода текста и проверка его длинны - добавлена логика ввода пути до файла и проверка корректности введенного пути

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        int count = 1;
+        boolean pathCorrect = false;
+        while(!pathCorrect){
+            System.out.println("Введите путь к файлу:");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (isDirectory){
+                System.out.println("Указанный путь является путём к папке, а не к файлу" + "\n");
+                count++;
+                continue;
+            }
+            if (!fileExists) {
+                System.out.println("Указанного файла не существует" + "\n");
+                count++;
+                continue;
+            }
+            System.out.println("Путь указан верно. Это файл номер " + count);
+            pathCorrect = true;
+        }
     }
 }
